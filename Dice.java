a204690a763f98a19929b0076e3f61e5847a7776
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-10 10:27:00 +0530

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,21 @@
+package com.recursion;
+
+public class Dice {
+    public static void main(String[] args) {
+     print("",4);
+    }
+
+    static void print(String p, int tar){
+        //common statement in pattern questions
+        if(tar == 0){
+            System.out.println(p);
+            return;
+        }
+        for(int i = 1; i<=6 && i <=tar; i++){
+            print(p + i,tar -i);
+        }
+
+
+
+    }
+}
