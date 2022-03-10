ddd7594b64930d80869293e607df5938208d0036
Saksham Guleria <65389594+Saksham369@users.noreply.github.com>
2022-03-10 10:25:59 +0530

    Add files via upload
    Asked in Amazone

--- PhonePad.java
+++ PhonePad.java
@@ -1,22 +1,25 @@
 package com.recursion;
-
+import java.util.*;
 public class PhonePad {
     public static void main(String[] args) {
-        pad("","12");
+       System.out.println( pad("","12"));
     }
-    static void pad(String p, String up){
+    static ArrayList<String> pad(String p, String up){
         if(up.isEmpty()){
-            System.out.println(p);
-            return;
+            ArrayList<String> s = new ArrayList<>(); //this is the main approach
+            s.add(p);
+            return s;
         }
 
         int digit =up.charAt(0) - '0';//By subtracting it with the ASCII value of the '0' we convert the char to int
+        ArrayList<String> s = new ArrayList<>();
+
         for(int i = (digit-1) * 3; i < digit *3; i++){
             char c = (char)('a' + i);
-            pad(p+c,up.substring(1));
+            s.addAll(pad(p+c,up.substring(1)));
 
 
         }
-
+return s;
     }
 }
